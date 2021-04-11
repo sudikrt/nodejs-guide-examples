b7db21bbbd7b51b32191cdad00196da66400c21a
sudikrt <sudarshankaranth18@gmail.com>
2021-04-11 15:04:28 +0530

    handling routes

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 //root file node js file execute
 const express = require ('express');
+const bodyParser = require ('body-parser');
 
 const app = express (); //express as function
 
-//next is function to allow the request to travel on next middle ware
-app.use ( (req, res, next) => {
-    console.log ('In middle ware')
-    next (); // we need to allow otehr middlware to execute . Allow request to continue to next middle ware
+//app.use (express.json ()); // registers a midlleware   the request body parsing
+app.use (bodyParser.urlencoded ({extended : false})); // registers a midlleware   the request body parsing
 
+//before handling generic middle ware, it matches the url after the / if curent middle ware doesn't match
+//it will navigate the default middle ware
+app.use ('/add-product', (req, res, next) => {
+    res.send (`<h1>Add Product</h1>
+        <form method="POST" action="/product">
+            <input type="text" name="title"/>
+            <input type="submit" 
+        </form>
+    `);
 });
+app.post ('/product', (req, res, next)=> {
+    console.log (req.body);
+    res.redirect ('/');
+})
 
-app.use ( (req, res, next) => {
-    console.log ('In another middle ware')
+// when we say '/ (slash) ' if we make the ur; localhose:5001/add product it will land to same
+app.use ('/', (req, res, next) => {
     res.send ('<h1>Hello</h1>');
 });
 
