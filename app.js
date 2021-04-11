1e9b0ec3632c6c05f72ad21cfb2021f3b4286d5b
sudikrt <sudarshankaranth18@gmail.com>
2021-04-11 12:19:21 +0530

    Installing expess and adding multiple middle ware

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 const http = require('http');
 
-const routes = require ('./route');
+const express = require ('express');
 
-console.log (routes.someText);
+const app = express (); //express as function
 
-const server = http.createServer(routes.handler);
+//next is function to allow the request to travel on next middle ware
+app.use ( (req, res, next) => {
+    console.log ('In middle ware')
+    next (); // we need to allow otehr middlware to execute . Allow request to continue to next middle ware
+
+});
+
+app.use ( (req, res, next) => {
+    console.log ('In another middle ware')
+    res.send ('<h1>Hello</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(5001);``
