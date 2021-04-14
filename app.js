340ff9089c2a7b1848bb51ec8955a8d1fb5397d5
sudikrt <sudarshankaranth18@gmail.com>
2021-04-14 23:32:09 +0530

    istalling pug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require ('path');
 
 const app = express (); //express as function
 
+app.set ('view engine', 'pug');
 const adminRoutes =  require ('./routes/admin');
 const shopRoutes =  require ('./routes/shop');
 
@@ -11,7 +12,7 @@ const shopRoutes =  require ('./routes/shop');
 app.use (bodyParser.urlencoded ({extended : false})); // registers a midlleware   the request body parsing
 
 app.use (express.static (path.join (__dirname, 'public')))
-app.use ('/admin', adminRoutes);
+app.use ('/admin', adminRoutes.routes);
 app.use (shopRoutes);
 
 app.use ('/', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const path = require ('path');
 const express = require ('express');
 const router = express.Router ();
-
+const adminData = require ('./admin');
 const rootDir = require ('../utils/path');
 
 
 router.get ('/', (req, res, next) => {
-    res.sendFile (path.join (rootDir, 'views', 'shop.html'));
+    console.log (adminData.products);
+    return res.render ('shop');
 });
 
 module.exports = router;
