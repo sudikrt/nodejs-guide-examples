180dea108bfc206eef2006212f969bd10fb49ec9
sudikrt <sudarshankaranth18@gmail.com>
2021-04-23 00:54:45 +0530

    Added endpoints

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,11 @@ const path = require ('path');
 const express = require ('express');
 const router = express.Router ();
 const adminData = require ('./admin');
-const productsController = require ('../controllers/products');
+const shopController = require ('../controllers/shop');
 
 
-router.get ('/', productsController.getProducts);
-
+router.get ('/', shopController.getIndex);
+router.get ('/products', shopController.getProducts);
+router.get ('/cart', shopController.getCart);
+router.get ('/checkout', shopController.getCheckout);
 module.exports = router;
