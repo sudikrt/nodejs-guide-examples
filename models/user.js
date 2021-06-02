9a60d1febe293553f69db5c3ea802fb770fe2eb0
sudikrt <sudarshankaranth18@gmail.com>
2021-06-02 19:56:47 +0530

    Adding order functionality

--- models/user.js
+++ models/user.js
@@ -42,6 +42,71 @@ class User {
             }
         );
     }
+    getCart () {
+        const db = dbRef ();
+        const productIds = this.cart.items.map (item => item.productId);
+        return db.collection ('products')
+        .find ({_id : {$in : productIds}})
+        .toArray ()
+        .then (products => {
+            return products.map (eachProduct => 
+                {
+                    return {
+                        ...eachProduct, 
+                        quantity : this.cart.items.find (i => i.productId.toString () === eachProduct._id.toString ()).quantity
+                    };
+                });
+        }); 
+    }
+    deleteItemFromCart (productId) {
+        const updatedCartItems = this.cart.items.filter (ele => ele.productId.toString () !== productId.toString ());
+
+        const db = dbRef ();
+        const updatedCart = {items : updatedCartItems};
+        return db.collection ('users')
+        .updateOne (
+            {_id : new ObjectId (this._id)}, 
+            {
+                $set: {
+                    cart : updatedCart
+                }
+            }
+        );
+    }
+    createOrder () {
+        const db = dbRef ();
+        return this.getCart ().then (products => {
+            const order ={
+                items : products,
+                user : {
+                    _id : new ObjectId (this._id),
+                    name : this.name,
+                    email : this.email
+                }
+            };
+            return db.collection ('orders')
+                .insertOne (order)
+        }).then (result => {
+            this.cart.items = [];
+            return db
+            .collection ('users')
+            .updateOne (
+                {_id : new ObjectId (this._id)}, 
+                {
+                    $set: {
+                        cart : {items : []}
+                    }
+                }
+            );
+        })
+    }
+    getOrders () {
+        const db = dbRef ();
+        return db
+        .collection ('orders')
+        .find ({'user._id': new ObjectId (this._id)})
+        .toArray ();
+    }
     save () {
         const db = dbRef ();
         return db.collection ('users').insertOne (this);
