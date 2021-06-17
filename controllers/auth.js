d13fc8c84ee081fabb249f9e5c57dd622b504f87
sudikrt <sudarshankaranth18@gmail.com>
2021-06-17 23:02:50 +0530

    added cookies and auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
 exports.getLogin = (req, res, next) => {
+    let cookies = {};
+    req.get ('Cookie').split (';').forEach (ele => {
+        const tempResp  = ele.trim ().split ("=");
+        cookies[tempResp[0]] = tempResp[1];
+    });
+    const isLoggedIn = cookies.loggedIn;
+    
+
     return res.render ('auth/login', {
         docTitle : 'Login', 
-        path : '/login', 
+        path : '/login',
+        isAuthenticated : isLoggedIn
     });
+}
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader ('Set-Cookie', 'loggedIn=true');
+    res.redirect ('/');
 }
