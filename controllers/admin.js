0b4cc6b951cc12558d6cd17d5313e7bc4c491f50
sudikrt <sudarshankaranth18@gmail.com>
2021-07-06 23:49:07 +0530

    Added session storage for cookies

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,7 @@ exports.getAddProduct = (req, res, next) => {
             activeAddProduct : true, 
             formCSS : true,
             editing : false,
-            isAuthenticated : req.isLoggedIn
+            isAuthenticated : req.session.isLoggedIn
         }
     );
 }
@@ -24,7 +24,7 @@ exports.addNewProduct = (req, res, next)=> {
             price : price, 
             description : description, 
             imgUrl : imgUrl,
-            userId : req.user._id // req.user
+            userId : req.session.user
         }
     );
     
@@ -49,7 +49,7 @@ exports.getEditProduct = (req, res, next) => {
                 path:'/admin/edit-product', 
                 editing : editMode,
                 product : product,
-                isAuthenticated : req.isLoggedIn
+                isAuthenticated : req.session.isLoggedIn
             }
         );
     }).catch (err => {
@@ -68,7 +68,7 @@ exports.getProducts = (req, res, next) => {
             prods : products, 
             docTitle : 'Admin Products', 
             path : '/admin/products',
-            isAuthenticated : req.isLoggedIn
+            isAuthenticated : req.session.isLoggedIn
         });
     }).catch ( errors => {
         console.log ('err : ' + errors)
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,39 @@
 const User = require ('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    let cookies = {};
-    if (req.get ('Cookie')) {
-        req.get ('Cookie').split (';').forEach (ele => {
-            const tempResp  = ele.trim ().split ("=");
-            cookies[tempResp[0]] = tempResp[1];
-        });
-    }
+    // let cookies = {};
+    // if (req.get ('Cookie')) {
+    //     req.get ('Cookie').split (';').forEach (ele => {
+    //         const tempResp  = ele.trim ().split ("=");
+    //         cookies[tempResp[0]] = tempResp[1];
+    //     });
+    // }
 
-    console.log ('***** :' + req.session.loggedIn);
-    const isLoggedIn = cookies.loggedIn;
+    // console.log ('***** :' + req.session.loggedIn);
+    // const isLoggedIn = cookies.loggedIn;
     
     console.log (req.session);
     return res.render ('auth/login', {
         docTitle : 'Login', 
         path : '/login',
-        isAuthenticated : isLoggedIn
+        isAuthenticated : false
     });
 }
 
 exports.postLogin = (req, res, next) => {
     console.log (req.session);
     //res.setHeader ('Set-Cookie', 'loggedIn=true; Max-age=10; Secure');
-    app.use ((req,res, next) => {
-        User.findById ("60b5daf8307517723e29cc91").then (user => {
-            req.session.user = user;
-            req.session.loggedIn = true;
-            res.redirect ('/');
-        }).catch (error => {
-            console.log (error);
-        })
+    User.findById ("60b5daf8307517723e29cc91").then (user => {
+        req.session.user = user;
+        req.session.isLoggedIn = true;
+        res.redirect ('/');
+    }).catch (error => {
+        console.log (error);
+    })
+}
+exports.postLogout = (req, res, next) => {
+    req.session.destroy ( error => {
+        console.log (error);
+        res.redirect ('/');
     })
 }
