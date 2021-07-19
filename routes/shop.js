c1e783fc7f676e38c05bb997888f67fa802b405d
sudikrt <sudarshankaranth18@gmail.com>
2021-07-19 23:55:33 +0530

    added auth check

--- routes/shop.js
+++ routes/shop.js
@@ -3,15 +3,16 @@ const express = require ('express');
 const router = express.Router ();
 const adminData = require ('./admin');
 const shopController = require ('../controllers/shop');
+const isAuth = require ('../middleware/is-auth');
 
 
 router.get ('/', shopController.getIndex);
 router.get ('/products', shopController.getProducts);
 router.get ('/products/:productId', shopController.getProduct);
-router.get ('/cart', shopController.getCart);
-router.post ('/cart', shopController.postCart);
-router.post ('/create-order', shopController.postOrder);
-router.post ('/cart-delete-item', shopController.postCartDeleteItem);
-router.get ('/orders', shopController.getOrders);
+router.get ('/cart', isAuth, shopController.getCart);
+router.post ('/cart', isAuth, shopController.postCart);
+router.post ('/create-order', isAuth, shopController.postOrder);
+router.post ('/cart-delete-item', isAuth, shopController.postCartDeleteItem);
+router.get ('/orders', isAuth, shopController.getOrders);
 //router.get ('/checkout', shopController.getCheckout);
 module.exports = router;
